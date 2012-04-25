f9b55b0d80a7038fdd55120d7de0c054c4f26b21
TParis <TParis.wiki@gmail.com>
2012-04-25 02:05:09 +0000

    Made regex less greedy

--- usgs-bot.php
+++ usgs-bot.php
@@ -26,24 +26,25 @@ foreach ($i as $page) {
 	$makeChange = true;
 
 	//Find any {{coor dms}}, {{coor at dm}} or {{coor at dms}} and convert to {{coord}}
-	$pattern = "/\{\{(coor dms|coor dm|coor at dm|coor at dms)\|(\d*)\|(\d*)\|(N|S)\|(\d*)\|(\d*)\|(E|W)\|([a-zA-Z:_\.]*)\|(.*)\}\}/";
+	$pattern = "/\{\{(coor dms|coor dm|coor at dm|coor at dms)\|(\d{0,2})\|(\d{0,2})\|(N|S)\|(\d{0,2})\|(\d{0,2})\|(E|W)\|([a-zA-Z,=:_\.]*)\|([a-zA-Z0-9,\._=\|]*)\}\}/";
 	$replace = "{{coord|$2|$3|$4|$5|$6|$7|$8|$9}}";
 	$content = preg_replace($pattern, $replace, $content);
 
 
 	//Check whether a title and inline coord already exists
-	$pattern = "/\{\{coord(\|.*)*display=(title|title,inline|inline,title)\|(.*)\}\}/";
+	$pattern = "/\{\{coord([a-zA-Z0-9:=\|\.]*)display=(title|title,inline|inline,title)([a-zA-Z0-9=\|\.]*)\}\}/";
 
 	if (preg_match($pattern, $content) > 0) {
 		$makeChange = false;
 	}
 
 	if ($makeChange) {
-		$pattern = "/\{\{coord(\|.*)*display=inline\|(.*)\}\}/";
+		$pattern = "/\{\{coord([a-zA-Z0-9:=\|]*)display=inline\|([a-zA-Z0-9=\|\.]*)\}\}/";
 		$replace = "{{coord$1display=inline,title|$2}}";
 		$content = preg_replace($pattern,$replace,$content, 1);
 
 	}
+
 
 	$page->edit($content, "Converting Coord templates, setting format to inline and title", true);
 
