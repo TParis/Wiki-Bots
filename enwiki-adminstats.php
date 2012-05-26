2e194e9db04843426a6f424f953675fae915c3d8
cyberpower678 <cybernet678@yahoo.com>
2012-05-25 22:46:29 -0300

    Adding a patch.  This patch has not been tested yet so results may be unexpected.

--- enwiki-adminstats.php
+++ enwiki-adminstats.php
@@ -18,15 +18,23 @@ $site->set_runpage("User:TPBot/Run/Adminstats");
 
 $u = initPage('Template:Adminstats')->embeddedin( array( 2, 3 ) );
 
+$admins = $site->allusers( null, array( 'sysop' ), null, false, array( 'blockinfo', 'groups', 'editcount', 'registration' ), null );
 
 shuffle($u);
 // = array("User talk:X!");
 
 foreach ($u as $name) {
-        preg_match("/User( talk)?:([^\/]*)/i", $name, $m);
-        process($m[2]);
+   	preg_match("/User( talk)?:([^\/]*)/i", $name, $m);
+		if( in_array( $m[2], $admins['name'] ) ) {
+			process($m[2]);
+		}
+		else {
+			$out = "'''{{BASEPAGENAME}} is not an administrator.\nTherefore they have disallowed the use of adminstats.'''";
+			echo $out;
+			echo "\n";
+			$toedit = "Template:Adminstats/$m[2]";
+			initPage( $toedit )->edit($out,"Adminstats are not allowed for this user.",true);
 }
-
 function process ($rawuser) {
     global $site, $db;
     
@@ -37,46 +45,46 @@ function process ($rawuser) {
     $out = "{{Adminstats/Core\n|edits=$livecount\n|ed=$editcount\n";
     
     $uid = $db->select(
-      'user',
-      'user_id',
-      array(
-         'user_name' => $rawuser
-      )
+    	'user',
+    	'user_id',
+    	array(
+    		'user_name' => $rawuser
+    	)
     );
     $uid = $uid[0]['user_id'];
     if( !$uid ) return;
     
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_type' => 'newusers'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_type' => 'newusers'
+    	)
     );
     if( !$res ) return;
     
     $out .= "|created={$res[0]['count']}\n";
-     
+    
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_action' => 'delete'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_action' => 'delete'
+    	)
     );
     if( !$res ) return;
     
     $out .= "|deleted={$res[0]['count']}\n";
     
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_action' => 'restore'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_action' => 'restore'
+    	)
     );
     if( !$res ) return;
     
@@ -84,72 +92,72 @@ function process ($rawuser) {
     
      
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_action' => 'block'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_action' => 'block'
+    	)
     );
     if( !$res ) return;
     
     $out .= "|blocked={$res[0]['count']}\n";
     
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_action' => 'protect'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_action' => 'protect'
+    	)
     );
     if( !$res ) return;
     
     $out .= "|protected={$res[0]['count']}\n";
     
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_action' => 'unprotect'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_action' => 'unprotect'
+    	)
     );
     if( !$res ) return;
     
     $out .= "|unprotected={$res[0]['count']}\n";
     
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_action' => 'rights'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_action' => 'rights'
+    	)
     );
     if( !$res ) return;
     
     $out .= "|rights={$res[0]['count']}\n";
     
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_action' => 'reblock'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_action' => 'reblock'
+    	)
     );
     if( !$res ) return;
     
     $out .= "|reblock={$res[0]['count']}\n";
         
     $res = $db->select(
-      'logging',
-      'count(log_action) AS count',
-      array(
-         'log_user' => $uid,
-         'log_action' => 'modify'
-      )
+    	'logging',
+    	'count(log_action) AS count',
+    	array(
+    		'log_user' => $uid,
+    		'log_action' => 'modify'
+    	)
     );
     if( !$res ) return;
     
